0effd7996a3ec8f1e2e6c7445e91e41953b2ff30
youjiaqi <2430284055@qq.com>
2023-02-20 09:45:22 +0800

    feat : 级联Group

--- src/view/excitation/excitationListFrom/groupExcitationFormGroup.tsx
+++ src/view/excitation/excitationListFrom/groupExcitationFormGroup.tsx
@@ -1,34 +1,25 @@
 import { PlusOutlined } from '@ant-design/icons'
 import { Form, Input } from 'antd'
 import { useForm } from 'antd/lib/form/Form'
-
 import * as React from 'react'
 import { useContext, useState } from 'react'
 import { useHistory } from 'react-router'
 import CommonButton from 'Src/components/Button/commonButton'
 import { GlobalContexted } from 'Src/components/globalBaseMain/globalBaseMain'
-import { createGroupFn, excitationListFn } from 'Src/services/api/excitationApi'
+import { createDoubleExcitationFn, excitationListFn } from 'Src/services/api/excitationApi'
 import { throwErrorMessage } from 'Src/util/message'
+
 import styles from '../excitation.less'
 import ExcitationCard from '../excitationComponent/excitationCard'
 import { GetDeatilFn } from './getDataDetailFn/getDataDetailFn'
+// import { RouteComponentProps, StaticContext } from 'react-router'
 
 const layout = {
-  labelCol: { span: 4 },
+  labelCol: { span: 5 },
   wrapperCol: { span: 18 }
 }
 
-const oneRequest = {
-  target_type: '0',
-  key_word: '',
-  status: null,
-  page: 1,
-  page_size: 999,
-  sort_field: 'create_time',
-  sort_order: 'descend'
-}
-
-const doubleRequest = {
+const request = {
   target_type: 1,
   key_word: '',
   status: null,
@@ -37,15 +28,20 @@ const doubleRequest = {
   sort_field: 'create_time',
   sort_order: 'descend'
 }
-interface Option {
-  sender_id: string
+
+interface projectInfoType {
+  id: number
   name: string
-  disabled?: boolean
-  children?: any[]
+  port: string
+  status: number | null
+  create_time: string
+  update_time: string
+  create_user: string
+  update_user: string
 }
 
 interface Resparams {
-  target_type: number | string
+  target_type: number
   key_word?: string
   status?: null | number
   page: number
@@ -57,31 +53,29 @@ interface formPorps {
   [key: number]: any
 }
 const GroupExcitationFormGroup: React.FC = () => {
-  const { isFixForm, info, type } = useContext(GlobalContexted)
-  const history = useHistory()
   const [form] = useForm()
+  const history = useHistory()
+  const { isFixForm, info, type } = useContext(GlobalContexted)
+  const [excitationList, setExcitationList] = useState<projectInfoType[]>([])
   const [cardArray, setCardArray] = React.useState(1)
+  const [data, setData] = useState<number[]>([])
   const [cardCheckStatus, setCardCheckStatus] = useState(true)
   const [isDisableStatus, setIsDisableStatus] = React.useState<boolean>(true)
-  const [excitationList, setExcitationList] = useState<Option[]>([
-    {
-      sender_id: '0',
-      name: '单激励',
-      disabled: false,
-      children: []
-    },
-    {
-      sender_id: '1',
-      name: '级联激励',
-      disabled: false,
-      children: []
-    }
-  ])
-  const [data, setData] = useState<number[]>([])
   const Data = GetDeatilFn(info?.id)
   const addCard = React.useCallback(() => {
     setCardArray(pre => pre + 1)
   }, [])
+  const getExcitationList = async (value: Resparams) => {
+    try {
+      const result = await excitationListFn(value)
+      if (result.data) {
+        setExcitationList(result.data.results)
+      }
+    } catch (error) {
+      throwErrorMessage(error, { 1004: '请求资源未找到' })
+    }
+  }
+
   const ExcitationCardList = React.useMemo(() => {
     const data = Array.from({ length: cardArray }, (v, i) => i)
     return data
@@ -98,42 +92,6 @@ const GroupExcitationFormGroup: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [data]
   )
-  const cancelForm = () => {
-    history.push({
-      pathname: '/excitationList',
-      state: { type }
-    })
-  }
-  const getExcitationList = async (oneRequest: Resparams, doubleRequest: Resparams) => {
-    try {
-      const result1 = await excitationListFn(oneRequest)
-      const result2 = await excitationListFn(doubleRequest)
-      Promise.all([result1, result2])
-        .then(value => {
-          const result1Data = value[0].data?.results
-          const result2Data = value[1].data?.results
-          setExcitationList(pre => {
-            const preCopy = pre
-            if (isFixForm) {
-              return [
-                { ...preCopy[0], disabled: isFixForm, children: result1Data },
-                { ...preCopy[1], disabled: isFixForm, children: result2Data }
-              ]
-            }
-            return [
-              { ...preCopy[0], children: result1Data },
-              { ...preCopy[1], children: result2Data }
-            ]
-          })
-          return value
-        })
-        .catch(error => {
-          throwErrorMessage(error)
-        })
-    } catch (error) {
-      throwErrorMessage(error, { 1004: '请求资源未找到' })
-    }
-  }
 
   const createOneExcitationFn = React.useCallback(async () => {
     let values
@@ -146,10 +104,17 @@ const GroupExcitationFormGroup: React.FC = () => {
       if (values) {
         const params = {
           name: values.name,
-          desc: values.description,
+          desc: values.name,
+          gu_cnt0: +values.gu_cnt0,
+          gu_w0: +values.gu_w0,
+          gu_cnt1: +values.gu_cnt1,
+          gu_w1: +values.gu_w1,
+          align_delay_0: +values.align_delay_0,
+          align_delay_1: +values.align_delay_1,
+          align_delay_2: +values.align_delay_2,
           child_id_list: data
         }
-        const result = await createGroupFn(params)
+        const result = await createDoubleExcitationFn(params)
         if (result.data) {
           history.push({
             pathname: '/excitationList',
@@ -158,9 +123,10 @@ const GroupExcitationFormGroup: React.FC = () => {
         }
       }
     } catch (error) {
-      throwErrorMessage(error, { 1009: '项目删除失败' })
+      throwErrorMessage(error)
     }
   }, [form, data, history, type])
+
   const getLength = React.useCallback(() => {
     if (data.length === 0) return false
     const bol = Object.values(data).every(item => {
@@ -168,6 +134,13 @@ const GroupExcitationFormGroup: React.FC = () => {
     })
     return bol
   }, [data])
+
+  const cancelForm = () => {
+    history.push({
+      pathname: '/excitationList',
+      state: { type }
+    })
+  }
   const onFieldsChange = (changedFields: any, allFields: any) => {
     const disabledData: any = []
     const errors = allFields.every((item: any) => {
@@ -187,6 +160,7 @@ const GroupExcitationFormGroup: React.FC = () => {
       setIsDisableStatus(true)
     }
   }
+
   React.useEffect(() => {
     if (!getLength() && isDisableStatus) {
       setCardCheckStatus(true)
@@ -196,17 +170,22 @@ const GroupExcitationFormGroup: React.FC = () => {
   }, [data, getLength, isDisableStatus])
 
   React.useEffect(() => {
-    getExcitationList(oneRequest, doubleRequest)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isFixForm])
-
+    getExcitationList(request)
+  }, [])
   React.useEffect(() => {
     if (Data && isFixForm) {
       setCardArray(Data?.group_data_list.length)
-      const { name, desc } = Data as any
+      const { name, desc, gu_cnt0, gu_w0, gu_w1, gu_cnt1, align_delay_0, align_delay_2, align_delay_1 } = Data as any
       const formData = {
         name,
-        description: desc
+        description: desc,
+        gu_cnt0,
+        gu_w0,
+        gu_cnt1,
+        gu_w1,
+        align_delay_0,
+        align_delay_1,
+        align_delay_2
       }
       form.setFieldsValue(formData)
     }
@@ -215,7 +194,7 @@ const GroupExcitationFormGroup: React.FC = () => {
     <div className={styles.baseForm}>
       <Form name='basic' className={styles.twoForm} {...layout} onFieldsChange={onFieldsChange} autoComplete='off' form={form} size='large'>
         <Form.Item
-          label='交互名称'
+          label='级联Group名称'
           name='name'
           validateFirst
           validateTrigger={['onBlur']}
@@ -224,7 +203,7 @@ const GroupExcitationFormGroup: React.FC = () => {
               validateTrigger: 'onBlur',
               validator(_, value) {
                 if (typeof value === 'undefined' || value === '') {
-                  return Promise.reject(new Error('请输入旁路名称'))
+                  return Promise.reject(new Error('请输入级联Group名称'))
                 }
                 return Promise.resolve()
               }
@@ -233,7 +212,7 @@ const GroupExcitationFormGroup: React.FC = () => {
               required: true,
               max: 20,
               min: 2,
-              message: '交互名称长度为2到20个字符'
+              message: '级联Group名称长度为2到20个字符'
             },
             {
               validateTrigger: 'onBlur',
@@ -242,22 +221,189 @@ const GroupExcitationFormGroup: React.FC = () => {
                 if (reg.test(value)) {
                   return Promise.resolve()
                 }
-                return Promise.reject(new Error('任务名称由汉字、数字、字母和下划线组成'))
+                return Promise.reject(new Error('级联Group名称由汉字、数字、字母和下划线组成'))
               }
             }
           ]}
         >
-          <Input disabled={isFixForm} placeholder='请输入2到20个字符' />
+          <Input placeholder='请输入级联Group名称' disabled={isFixForm} />
         </Form.Item>
-
         <Form.Item
-          label='交互描述'
+          label='前置时延'
+          name='align_delay_0'
+          validateFirst
+          validateTrigger={['onBlur']}
+          rules={[
+            {
+              required: true,
+              validateTrigger: 'onBlur',
+              validator(_, value) {
+                const reg = /^\d+$/
+                if (reg.test(value)) {
+                  if (value <= 100) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('请输入 0-100 之间的整数'))
+                }
+                return Promise.reject(new Error('请输入 0-100 之间的整数'))
+              }
+            }
+          ]}
+        >
+          <Input disabled={isFixForm} placeholder='请输入前置时延' suffix='毫秒' />
+        </Form.Item>
+        <Form.Item
+          label='发送次数'
+          name='gu_cnt0'
+          validateFirst
+          validateTrigger={['onBlur']}
+          rules={[
+            {
+              required: true,
+              validateTrigger: 'onBlur',
+              validator(_, value) {
+                const reg = /^\d+$/
+                if (reg.test(value)) {
+                  if (value > 0 && value <= 20) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('请输入 1-20 之间的整数'))
+                }
+                return Promise.reject(new Error('请输入 1-20 之间的整数'))
+              }
+            }
+          ]}
+        >
+          <Input placeholder='请输入循环次数' disabled={isFixForm} />
+        </Form.Item>
+        <Form.Item
+          label='等待时间'
+          name='gu_w0'
+          validateFirst
+          validateTrigger={['onBlur']}
+          rules={[
+            {
+              required: true,
+              validateTrigger: 'onBlur',
+              validator(_, value) {
+                const reg = /^\d+$/
+                if (reg.test(value)) {
+                  if (value <= 10) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('请输入 0-10 之间的整数'))
+                }
+                return Promise.reject(new Error('请输入 0-10 之间的整数'))
+              }
+            }
+          ]}
+        >
+          <Input placeholder='请输入等待时间' suffix='毫秒' disabled={isFixForm} />
+        </Form.Item>
+        <Form.Item
+          label='循环次数'
+          name='gu_cnt1'
+          validateFirst
+          validateTrigger={['onBlur']}
+          rules={[
+            {
+              required: true,
+              validateTrigger: 'onBlur',
+              validator(_, value) {
+                const reg = /^\d+$/
+                if (reg.test(value)) {
+                  if (value > 0 && value <= 20) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('请输入 1-20 之间的整数'))
+                }
+                return Promise.reject(new Error('请输入 1-20 之间的整数'))
+              }
+            }
+          ]}
+        >
+          <Input placeholder='请输入循环次数' disabled={isFixForm} />
+        </Form.Item>
+        <Form.Item
+          label='循环间隔'
+          name='gu_w1'
+          validateFirst
+          validateTrigger={['onBlur']}
+          rules={[
+            {
+              required: true,
+              validateTrigger: 'onBlur',
+              validator(_, value) {
+                const reg = /^\d+$/
+                if (reg.test(value)) {
+                  if (value <= 10) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('请输入 0-10 之间的整数'))
+                }
+                return Promise.reject(new Error('请输入 0-10 之间的整数'))
+              }
+            }
+          ]}
+        >
+          <Input placeholder='请输入循环间隔' suffix='毫秒' disabled={isFixForm} />
+        </Form.Item>
+        <Form.Item
+          label='中间时延'
+          name='align_delay_1'
+          validateFirst
+          validateTrigger={['onBlur']}
+          rules={[
+            {
+              required: true,
+              validateTrigger: 'onBlur',
+              validator(_, value) {
+                const reg = /^\d+$/
+                if (reg.test(value)) {
+                  if (value <= 100) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('请输入 0-100 之间的整数'))
+                }
+                return Promise.reject(new Error('请输入 0-100之间的整数'))
+              }
+            }
+          ]}
+        >
+          <Input disabled={isFixForm} placeholder='请输入中间时延' suffix='毫秒' />
+        </Form.Item>
+        <Form.Item
+          label='后置时延'
+          name='align_delay_2'
+          validateFirst
+          validateTrigger={['onBlur']}
+          rules={[
+            {
+              required: true,
+              validateTrigger: 'onBlur',
+              validator(_, value) {
+                const reg = /^\d+$/
+                if (reg.test(value)) {
+                  if (value <= 100) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('请输入 0-100 之间的整数'))
+                }
+                return Promise.reject(new Error('请输入 0-100 之间的整数'))
+              }
+            }
+          ]}
+        >
+          <Input disabled={isFixForm} placeholder='请输入后置时延' suffix='毫秒' />
+        </Form.Item>
+        <Form.Item
+          label='级联描述'
           name='description'
-          rules={[{ message: '请输入交互描述!' }, { type: 'string', max: 50, message: '字数不能超过50个 ' }]}
+          rules={[{ message: '请输入级联Group描述!' }, { type: 'string', max: 50, message: '字数不能超过50个 ' }]}
         >
           <Input.TextArea
             disabled={isFixForm}
-            placeholder='交互描述'
+            placeholder='请输入级联Group描述'
             autoSize={{ minRows: 4, maxRows: 5 }}
             showCount={{
               formatter({ count }) {
@@ -267,31 +413,32 @@ const GroupExcitationFormGroup: React.FC = () => {
           />
         </Form.Item>
       </Form>
+
       <div className={styles.formOperation}>
         {ExcitationCardList?.map((index: number) => {
           return (
             <ExcitationCard
               type={type}
-              excitationList={excitationList}
-              isFixForm={isFixForm}
-              onChange={onChange}
               formData={Data?.group_data_list}
+              isFixForm={isFixForm}
+              excitationList={excitationList}
+              onChange={onChange}
               index={index}
               key={index}
             />
           )
         })}
-
         {ExcitationCardList.length < 10 && !isFixForm && (
           <div className={styles.nav_Btn} role='time' onClick={addCard}>
             <PlusOutlined style={{ fontSize: '20px', marginBottom: '3px' }} />
             <span>添</span>
             <span>加</span>
-            <span>激</span>
-            <span>励</span>
+            <span>级</span>
+            <span>联</span>
           </div>
         )}
       </div>
+
       <div className={styles.excitaion_footer}>
         <div className={styles.excitaion_footer_footerConcent}>
           {!isFixForm ? (
