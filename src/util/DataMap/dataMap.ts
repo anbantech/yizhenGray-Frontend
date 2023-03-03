64b5fc8283cd3c20ebbc274e954c938dec7da893
youjiaqi <2430284055@qq.com>
2023-03-03 16:36:33 +0800

    feat :  测试详情

--- src/util/DataMap/dataMap.ts
+++ src/util/DataMap/dataMap.ts
@@ -17,36 +17,37 @@ export const statusMap = {
     color: styles.roundStatusNormalEnd
   },
   1: {
-    label: '测试中',
-    color: styles.roundStatusTesting
-  },
-  2: {
-    label: '暂停',
-    color: styles.roundStatusPaused
-  },
-  3: {
-    label: '异常暂停',
-    color: styles.roundStatusErrorPaused
-  },
-  4: {
     label: '异常结束',
     color: styles.roundStatusErrorEnd
   },
+  2: {
+    label: '测试中',
+    color: styles.roundStatusTesting
+  },
+  3: {
+    label: '暂停',
+    color: styles.roundStatusPaused
+  },
+  4: {
+    label: '异常暂停',
+    color: styles.roundStatusErrorPaused
+  },
   5: {
+    label: '准备中',
+    color: styles.roundStatusReady
+  },
+  6: {
     label: '待测试',
     color: styles.roundStatusWaitTesting
   },
-  6: {
-    label: '重放中',
-    color: styles.roundStatusReplay
-  },
+
   7: {
     label: '停止中',
     color: styles.roundStatusStoping
   },
   8: {
-    label: '准备中',
-    color: styles.roundStatusReady
+    label: '重放中',
+    color: styles.roundStatusReplay
   },
   9: {
     label: '未知',
