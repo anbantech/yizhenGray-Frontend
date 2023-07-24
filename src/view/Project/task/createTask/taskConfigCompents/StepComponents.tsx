5242819e4ebeb1cc11b1bd20ac86a40116f26800
youjiaqi <2430284055@qq.com>
2023-07-24 17:16:28 +0800

    feat :  拖拽列表逻辑

--- src/view/Project/task/createTask/taskConfigCompents/StepComponents.tsx
+++ src/view/Project/task/createTask/taskConfigCompents/StepComponents.tsx
@@ -1,68 +1,389 @@
-import { Form, FormInstance, Input, Steps } from 'antd'
-import React, { useState } from 'react'
+import { Checkbox, Form, Input, Steps } from 'antd'
+import { CheckboxValueType } from 'antd/lib/checkbox/Group'
+import { DndProvider, DropTargetMonitor, useDrag, useDrop } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import React, { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { excitationListFn } from 'Src/services/api/excitationApi'
+import { throwErrorMessage } from 'Src/util/common'
+import dragImg from 'Src/assets/drag/icon_drag.png'
+import { useRequestStore } from 'Src/view/NewExcitation/ExcitaionStore/ExcitaionStore'
+import { DropTip } from 'Src/view/excitation/excitationComponent/Tip'
+import InputNumberSuffixMemo from 'Src/components/inputNumbersuffix/inputNumberSuffix'
+import stepStore from './sendListStore'
+import StyleSheetOther from '../../../../NewExcitation/ExcitationComponents/ExcitationDraw/excitationDraw.less'
 import StyleSheet from './stepBaseConfig.less'
 
 const layout = {
   wrapperCol: { span: 22 }
 }
+
+const CloumnLine = () => {
+  return <div className={StyleSheetOther.cloumnLine} />
+}
 const OneSteps = () => {
   const { TextArea } = Input
-  const [form] = Form.useForm<FormInstance>()
+  const setButtonStatus = stepStore(state => state.setButtonStatus)
+  const setBaseInfo = stepStore(state => state.setBaseInfo)
+  const baseInfo = stepStore(state => state.baseInfo)
+  const [form] = Form.useForm<any>()
   const onValuesChange = (changedValues: any, allValues: any) => {
     const bol = allValues.desc === undefined || allValues.desc?.length <= 50
     if (allValues.name?.length >= 2 && allValues.name.length <= 20 && /^[\w\u4E00-\u9FA5]+$/.test(allValues.name) && bol) {
+      const val = { name: allValues.name, desc: allValues.desc }
+      setBaseInfo(val)
+      setButtonStatus(false)
     } else {
+      setButtonStatus(true)
     }
   }
+  useEffect(() => {
+    if (baseInfo) {
+      const { name, desc } = baseInfo
+      const formData = { name, desc }
+      form.setFieldsValue(formData)
+    }
+  }, [baseInfo, form, setBaseInfo])
   return (
-    <>
-      <Form form={form} autoComplete='off' {...layout} className={StyleSheet.oneStepform} onValuesChange={onValuesChange}>
-        <Form.Item
-          name='name'
-          validateFirst
-          label='名称'
-          validateTrigger={['onBlur']}
-          rules={[
-            { required: true, message: '请输入项目名称' },
-            { type: 'string', min: 2, max: 20, message: '项目名称长度为2到20个字符' },
-            {
-              validateTrigger: 'onBlur',
-              validator(_, value) {
-                const reg = /^[\w\u4E00-\u9FA5]+$/
-                if (reg.test(value)) {
-                  return Promise.resolve()
-                }
-                return Promise.reject(new Error('项目名称由汉字、数字、字母和下划线组成'))
+    <Form form={form} autoComplete='off' {...layout} className={StyleSheet.oneStepform} onValuesChange={onValuesChange}>
+      <Form.Item
+        name='name'
+        validateFirst
+        label='名称'
+        validateTrigger={['onBlur']}
+        rules={[
+          { required: true, message: '请输入项目名称' },
+          { type: 'string', min: 2, max: 20, message: '项目名称长度为2到20个字符' },
+          {
+            validateTrigger: 'onBlur',
+            validator(_, value) {
+              const reg = /^[\w\u4E00-\u9FA5]+$/
+              if (reg.test(value)) {
+                return Promise.resolve()
               }
+              return Promise.reject(new Error('项目名称由汉字、数字、字母和下划线组成'))
             }
-          ]}
-        >
-          <Input placeholder='请输入项目名称' />
-        </Form.Item>
-        <Form.Item name='desc' label='描述' rules={[{ type: 'string', max: 50, message: '字数不能超过50个' }]}>
-          <TextArea
-            placeholder='请添加针对项目的相关描述'
-            autoSize={{ minRows: 4, maxRows: 5 }}
-            showCount={{
-              formatter({ count }) {
-                return `${count}/50`
-              }
-            }}
-          />
-        </Form.Item>
-      </Form>
-    </>
+          }
+        ]}
+      >
+        <Input placeholder='请输入项目名称' />
+      </Form.Item>
+      <Form.Item name='desc' label='描述' rules={[{ type: 'string', max: 50, message: '字数不能超过50个' }]}>
+        <TextArea
+          placeholder='请添加针对项目的相关描述'
+          autoSize={{ minRows: 4, maxRows: 5 }}
+          showCount={{
+            formatter({ count }) {
+              return `${count}/50`
+            }
+          }}
+        />
+      </Form.Item>
+    </Form>
   )
 }
+
 const TwoSteps = () => {
-  return <div>222</div>
-}
-const ThreeSteps = () => {
-  return <div>2222</div>
+  const [List, setList] = useState<Record<string, any>>([])
+  const CheckboxGroup = Checkbox.Group
+  const { params, setHasMore, hasMoreData, loadMoreData } = useRequestStore()
+  const setExcitation = stepStore(state => state.setExcitation)
+  const getExcitationList = React.useCallback(
+    async value => {
+      try {
+        const result = await excitationListFn(value)
+        if (result.data) {
+          const newList = [...result.data.results]
+          if (newList.length === 0) {
+            // InstancesDetail.setInstance(false)
+            setHasMore(false)
+            return false
+          }
+
+          if (newList.length === result.data.total) {
+            setHasMore(false)
+          }
+          setList([...newList])
+        }
+      } catch (error) {
+        throwErrorMessage(error, { 1004: '请求资源未找到' })
+      }
+    },
+    [setHasMore]
+  )
+
+  React.useEffect(() => {
+    getExcitationList(params)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params])
+
+  const onChange = (list: CheckboxValueType[]) => {
+    const listArray = list
+    const array = List.filter((item: any) => {
+      return listArray.includes(Number(item.sender_id))
+    })
+    setExcitation([...array])
+  }
+  return (
+    <div className={StyleSheet.twoSteps}>
+      <span className={StyleSheet.StepTitle}>激励列表</span>
+      <div id='scrollableDiv' className={StyleSheet.scrollDiv} style={{ height: 208 }}>
+        <CheckboxGroup onChange={onChange} style={{ width: '100%' }}>
+          <InfiniteScroll
+            dataLength={List.length}
+            next={loadMoreData}
+            hasMore={hasMoreData}
+            scrollableTarget='scrollableDiv'
+            loader={
+              <p style={{ textAlign: 'center', marginTop: '10px' }}>
+                {/* <div className={styles.listLine} /> */}
+                <div>内容已经加载完毕</div>
+              </p>
+            }
+            endMessage={
+              <p style={{ textAlign: 'center', marginTop: '10px' }}>
+                {/* <div className={styles.listLine} /> */}
+                <div>内容已经加载完毕</div>
+              </p>
+            }
+          >
+            {List?.map((item: any) => {
+              return (
+                <div key={`${item.sender_id}`} className={StyleSheet.StepItem}>
+                  <span>{item.name}</span>
+                  <div className={StyleSheet.checkBoxBody}>
+                    <Checkbox value={item.sender_id} />
+                  </div>
+                </div>
+              )
+            })}
+          </InfiniteScroll>
+        </CheckboxGroup>
+      </div>
+    </div>
+  )
 }
 
-function StepComponentsMemo() {
-  const [current, setCurrent] = useState(0)
+const DropableMemo = ({ index, item, moveCardHandler, sender_id, DropList, DeleteCheckItem }: any) => {
+  const ref = React.useRef<HTMLDivElement>(null)
+  const [{ isDragging }, drag] = useDrag(
+    () => ({
+      type: 'DragDropItem',
+      item: {
+        index,
+        sender_id
+      },
+      collect: monitor => ({
+        isDragging: monitor.isDragging()
+      })
+    }),
+    [sender_id, index, DropList]
+  )
+
+  const [{ handlerId }, drop] = useDrop({
+    accept: 'DragDropItem',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId()
+      }
+    },
+    hover(item: any, monitor: DropTargetMonitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+      // 拖拽元素下标与鼠标悬浮元素下标一致时，不进行操作
+      if (dragIndex === hoverIndex) {
+        return
+      }
+
+      // 确定屏幕上矩形范围
+      const hoverBoundingRect = ref.current!.getBoundingClientRect()
+
+      // 获取中点垂直坐标
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+      // 确定鼠标位置
+      const clientOffset = monitor.getClientOffset()
+
+      // 获取距顶部距离
+      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
+
+      /**
+       * 只在鼠标越过一半物品高度时执行移动。
+       *
+       * 当向下拖动时，仅当光标低于50%时才移动。
+       * 当向上拖动时，仅当光标在50%以上时才移动。
+       *
+       * 可以防止鼠标位于元素一半高度时元素抖动的状况
+       */
+
+      // 向下拖动
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+
+      // 向上拖动
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      // 执行 move 回调函数
+      moveCardHandler(dragIndex, hoverIndex)
+
+      /**
+       * 如果拖拽的组件为 Box，则 dragIndex 为 undefined，此时不对 item 的 index 进行修改
+       * 如果拖拽的组件为 Card，则将 hoverIndex 赋值给 item 的 index 属性
+       */
+
+      if (item.index !== undefined) {
+        // eslint-disable-next-line no-param-reassign
+        item.index = hoverIndex
+      }
+    }
+  })
+
+  drag(drop(ref))
+
+  return (
+    <div
+      ref={ref}
+      data-handler-id={handlerId}
+      className={StyleSheet.excitationItemDrop}
+      style={{ opacity: isDragging || item.isItemDragging ? 0.4 : 1, cursor: 'move' }}
+    >
+      <div className={StyleSheet.excitationItemDrop_left}>
+        <img className={StyleSheet.img_Body} src={dragImg} alt='' />
+        <Checkbox value={item.keys} />
+      </div>
+      <div>
+        <span>{index}</span>
+        <span>{item.name}</span>
+        <div style={{ paddingRight: '16px' }}>
+          <div role='time' onClick={() => {}} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Dropable = React.memo(DropableMemo)
+
+const ThreeSteps = () => {
+  const excitationList = stepStore(state => state.excitationList)
+
+  const [, drop] = useDrop(() => ({
+    accept: 'DragDropItem'
+  }))
+  const gu_cnt0 = stepStore(state => state.gu_cnt0)
+  const gu_w0 = stepStore(state => state.gu_w0)
+  const setValue = stepStore(state => state.setValue)
+  const onChangeGu_time = (type: string, e: React.ChangeEvent<HTMLInputElement>) => {
+    const newNumber = Number.parseInt(e.target.value || '0', 10)
+    if (Number.isNaN(newNumber)) {
+      return
+    }
+
+    setValue(type, newNumber)
+  }
+
+  const onMax = React.useCallback(
+    (type: string) => {
+      if (type === 'gu_cnt0') {
+        const newValue = Number(gu_cnt0) > 20 ? 20 : gu_cnt0
+        setValue(type, newValue)
+      }
+      if (type === 'gu_w0') {
+        const newValue = Number(gu_w0) > 100 ? 100 : gu_w0
+        setValue(type, newValue)
+      }
+    },
+    [gu_cnt0, gu_w0, setValue]
+  )
+
+  const moveCardHandler = React.useCallback(
+    (dragIndex: number, hoverIndex: number) => {
+      const dropCardListCopy = excitationList
+      dropCardListCopy.splice(dragIndex, 1, ...dropCardListCopy.splice(hoverIndex, 1, dropCardListCopy[dragIndex]))
+      // setLeftList([...dropCardListCopy])
+    },
+    [excitationList]
+  )
+
+  const DeleteCheckItem = React.useCallback(
+    index => {
+      const copyList = excitationList
+    },
+    [excitationList]
+  )
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className={StyleSheet.twoSteps}>
+        <div className={StyleSheet.ThreeStepTitle}>
+          <span className={StyleSheet.StepTitle}>激励列表</span>
+          <div style={{ display: 'flex', width: '400px', alignItems: 'center' }}>
+            <div className={StyleSheetOther.inputEdit} style={{ display: 'flex', alignItems: 'center' }}>
+              <span className={StyleSheetOther.headerDesc} style={{ marginRight: '8px', display: 'flex', width: 65 }}>
+                发送次数:
+              </span>
+              <Input
+                className={StyleSheetOther.numberInput}
+                value={gu_cnt0}
+                onBlur={() => {
+                  onMax('gu_cnt0')
+                }}
+                onChange={e => {
+                  onChangeGu_time('gu_cnt0', e)
+                }}
+                suffix={<InputNumberSuffixMemo type='gu_cnt0' />}
+              />
+            </div>
+            <CloumnLine />
+            <div className={StyleSheetOther.inputEdit} style={{ display: 'flex', alignItems: 'center' }}>
+              <span className={StyleSheetOther.headerDesc} style={{ marginRight: '8px', display: 'flex', width: 65 }}>
+                发送间隔:
+              </span>
+
+              <Input
+                className={StyleSheetOther.numberInput}
+                onChange={e => {
+                  onChangeGu_time('gu_w0', e)
+                }}
+                onBlur={() => {
+                  onMax('gu_w0')
+                }}
+                value={gu_w0}
+                suffix={<InputNumberSuffixMemo type='gu_w0' />}
+              />
+
+              <DropTip />
+            </div>
+          </div>
+        </div>
+
+        <div className={StyleSheet.scrollDiv} style={{ height: 208 }} ref={drop}>
+          {excitationList?.map((item: any, index: number) => {
+            return (
+              <Dropable
+                sender_id={item.sender_id}
+                key={`${item.sender_id}`}
+                DropList={excitationList}
+                moveCardHandler={moveCardHandler}
+                index={index}
+                DeleteCheckItem={DeleteCheckItem}
+                item={item}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </DndProvider>
+  )
+}
+
+function StepComponents() {
+  const current = stepStore(state => state.current)
   const { Step } = Steps
   const steps = [
     {
@@ -93,7 +414,5 @@ function StepComponentsMemo() {
     </div>
   )
 }
-
-const StepComponents = React.memo(StepComponentsMemo)
 
 export default StepComponents
