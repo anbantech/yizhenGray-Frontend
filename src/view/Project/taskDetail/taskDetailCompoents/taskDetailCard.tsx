5ef149734e6931046483f1647821e4cab44c027a
youjiaqi <2430284055@qq.com>
2023-08-30 21:00:23 +0800

    fix :  实列详情

--- src/view/Project/taskDetail/taskDetailCompoents/taskDetailCard.tsx
+++ src/view/Project/taskDetail/taskDetailCompoents/taskDetailCard.tsx
@@ -4,6 +4,7 @@ import { RightOutlined } from '@ant-design/icons'
 import * as React from 'react'
 import { ResTaskDetail } from 'Src/globalType/Response'
 import styles from '../taskDetail.less'
+import cardStyles from './taskCard.less'
 
 interface propsResTaskDetailType<T> {
   taskDetailInfo: T
@@ -12,6 +13,11 @@ interface propsResTaskDetailType<T> {
 
 interface cardProps {
   lookLog: () => void
+  value: string | number | null
+  concent: string
+}
+
+interface cardPropsCommon {
   value: string | number | null
   concent: string
 }
@@ -59,112 +65,110 @@ export const statusMap = {
     color: styles.roundStatusReplay
   },
   10: {
-    label: '未知',
-    color: styles.roundStatusBlue
+    label: '固件启动异常',
+    color: styles.roundStatusErrorEnd
   }
-}
-
-const CardComponents = (props: cardProps) => {
-  const { value, concent, lookLog } = props
-  return (
-    <div className={styles.taskDetailCard_Main}>
-      <div className={styles.card_Header}>
-        <span className={styles.cardConcent}>{concent}</span>
-        {concent === '用例总数(条)' ? (
-          <div
-            className={styles.taskDetailCard_Main_left_footer_detail}
-            role='log'
-            onClick={() => {
-              lookLog()
-            }}
-          >
-            <span>日志</span>
-            <RightOutlined />
-          </div>
-        ) : null}
-      </div>
-      {concent !== '状态' ? (
-        <span className={styles.cardTaskDetail}>{value}</span>
-      ) : value === 4 ? (
-        // <Badge.Ribbon text='异常暂停' color='red' style={{ top: '-25px' }}>
-        <div>
-          <span className={statusMap[(value as 0 | 1 | 2 | 3 | 4 | 5 | 6) || 0]?.color}>
-            {statusMap[(value as 0 | 1 | 2 | 3 | 4 | 5 | 6) || 0].label}
-          </span>
-        </div>
-      ) : (
-        // </Badge.Ribbon>
-        <div>
-          <span className={statusMap[(value as 0 | 1 | 2 | 3 | 4 | 5 | 6) || 0]?.color}>
-            {statusMap[(value as 0 | 1 | 2 | 3 | 4 | 5 | 6) || 0].label}
-          </span>
-        </div>
-      )}
-    </div>
-  )
 }
 
 type stringKey = Record<string, string>
 const showTitleMap = {
   test_num: '用例总数(条)',
-  error_num: 'Crash数量',
+  error_num: '缺陷数量',
   coverage: '覆盖率',
   test_speed: '测试速率(帧/秒)',
   test_time: '运行时长',
   status: '状态'
 }
 
+const TestNumComponentsMemo = (props: cardProps) => {
+  const { lookLog, value, concent } = props
+  return (
+    <div className={[cardStyles.testNum, cardStyles.commonCard].join(' ')}>
+      <div className={cardStyles.commonConcnet} style={{ display: 'flex', flexDirection: 'column' }}>
+        <span className={styles.cardConcent}>{concent}</span>
+        <span className={styles.cardTaskDetail}>{value}</span>
+      </div>
+      <div
+        className={styles.taskDetailCard_Main_left_footer_detail}
+        style={{ marginRight: '20px' }}
+        role='log'
+        onClick={() => {
+          lookLog()
+        }}
+      >
+        <span>日志</span>
+        <RightOutlined />
+      </div>
+    </div>
+  )
+}
+const TestNumComponents = React.memo(TestNumComponentsMemo)
+
+const CommonComponentsMemo = (props: cardPropsCommon) => {
+  const { value, concent } = props
+  return (
+    <div className={[cardStyles.commonCardConcent, cardStyles.commonCard].join(' ')}>
+      <span className={styles.cardConcent}>{concent}</span>
+      <span className={styles.cardTaskDetail}>{value}</span>
+    </div>
+  )
+}
+const CommonComponents = React.memo(CommonComponentsMemo)
+
+const CoverageCardMemo = (porps: Record<string, ResTaskDetail>) => {
+  const { taskMapInfo } = porps
+  return (
+    <div className={[cardStyles.coverageCommon, cardStyles.commonCard].join(' ')}>
+      <div className={styles.card_coverage}>
+        <span className={styles.cardConcent}>分支覆盖率</span>
+        <span className={styles.cardTaskDetail}>{taskMapInfo.branch_coverage}</span>
+      </div>
+      <div className={styles.card_line} />
+      <div style={{ paddingLeft: '10px' }} className={styles.card_coverage}>
+        <span className={styles.cardConcent}>语句覆盖率</span>
+        <span className={styles.cardTaskDetail}>{taskMapInfo.statement_coverage}</span>
+      </div>
+    </div>
+  )
+}
+const CoverageCard = React.memo(CoverageCardMemo)
+
+const StatusCardComponentsMemo = (props: cardPropsCommon) => {
+  const { value, concent } = props
+  return (
+    <div className={[cardStyles.commonCardConcent, cardStyles.commonCard].join(' ')}>
+      <span className={styles.cardConcent}>{concent}</span>
+      <span className={statusMap[(value as 0 | 1 | 2 | 3 | 4 | 5 | 6 | 10) || 0]?.color}>
+        {statusMap[(value as 0 | 1 | 2 | 3 | 4 | 5 | 6 | 10) || 0].label}
+      </span>
+    </div>
+  )
+}
+const StatusCardComponents = React.memo(StatusCardComponentsMemo)
+
 function TaskDetailCard(props: propsResTaskDetailType<ResTaskDetail>) {
   const taskMapInfo = props.taskDetailInfo
   const { lookLog } = props
-  const NumCard = () => {
-    return (
-      <div className={styles.coverage_main}>
-        <div className={styles.card_coverage}>
-          <span className={styles.cardConcent}>Crash数量</span>
-          <span className={styles.cardTaskDetail}>{taskMapInfo.error_num}</span>
-        </div>
-        <div className={styles.card_line} />
-        <div style={{ paddingLeft: '10px' }} className={styles.card_coverage}>
-          <span className={styles.cardConcent}>Warn数量</span>
-          <span className={styles.cardTaskDetail}>{taskMapInfo.warning_count || 0}</span>
-        </div>
-      </div>
-    )
-  }
 
-  const CoverageCard = () => {
-    return (
-      <div className={styles.coverage_main}>
-        <div className={styles.card_coverage}>
-          <span className={styles.cardConcent}>分支覆盖率</span>
-          <span className={styles.cardTaskDetail}>{taskMapInfo.branch_coverage}</span>
-        </div>
-        <div className={styles.card_line} />
-        <div style={{ paddingLeft: '10px' }} className={styles.card_coverage}>
-          <span className={styles.cardConcent}>语句覆盖率</span>
-          <span className={styles.cardTaskDetail}>{taskMapInfo.statement_coverage}</span>
-        </div>
-      </div>
-    )
-  }
   return (
     <div className={styles.taskDetailCard}>
       {Object.keys(showTitleMap).map((item: string) => {
         return (
-          <div key={Math.random()} className={item === 'coverage' || item === 'error_num' ? styles.cardBodys : styles.cardBody}>
-            {item === 'coverage' ? (
-              <CoverageCard />
-            ) : item === 'error_num' ? (
-              <NumCard />
-            ) : (
-              <CardComponents
+          <>
+            {item === 'test_num' ? (
+              <TestNumComponents
                 lookLog={lookLog}
                 concent={showTitleMap[item as keyof typeof showTitleMap]}
                 value={taskMapInfo[item as keyof typeof taskMapInfo]}
               />
+            ) : item === 'coverage' ? (
+              <CoverageCard taskMapInfo={taskMapInfo} />
+            ) : item === 'status' ? (
+              <StatusCardComponents concent={showTitleMap[item as keyof typeof showTitleMap]} value={taskMapInfo[item as keyof typeof taskMapInfo]} />
+            ) : (
+              <CommonComponents concent={showTitleMap[item as keyof typeof showTitleMap]} value={taskMapInfo[item as keyof typeof taskMapInfo]} />
             )}
-          </div>
+          </>
         )
       })}
     </div>
