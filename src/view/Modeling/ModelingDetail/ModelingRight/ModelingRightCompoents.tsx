45247108bfbe100e7fbf2395349ad260daaa13d7
youjiaqi <2430284055@qq.com>
2023-12-07 08:46:34 +0800

    bugfix : v1.3.001.20231210 数据更新

--- src/view/Modeling/ModelingDetail/ModelingRight/ModelingRightCompoents.tsx
+++ src/view/Modeling/ModelingDetail/ModelingRight/ModelingRightCompoents.tsx
@@ -864,6 +864,7 @@ const RegisterDetailsAttributes = () => {
                     checkoutBase_addreeAndLength(e.target.value, '寄存器', 'restore_value', checkNullAndHex)
                   }}
                   onBlur={e => {
+                    console.log('22')
                     onBlurAsyncCheckoutNameFormValues(e.target.value, '寄存器', 'restore_value', updateRegister, register)
                   }}
                 />
